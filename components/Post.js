3a18214cf38a53cb4d9e05e8296962f099f92831
9nongmaesot <111469298+9nongmaesot@users.noreply.github.com>
2022-12-06 01:34:26 +0700

    test 2

--- components/Post.js
+++ components/Post.js
@@ -3,25 +3,23 @@ import {View, Text, StyleSheet} from 'react-native';
 const Post = props => {
   return (
     <View style={styles.container}>
-      <Text style={styles.body}>
-        {props.studentName}
-      </Text>
+      <Text style={styles.body}>{props.studentName}</Text>
     </View>
-  )
-}
+  );
+};
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor:"LightGray",
-        margin: 10,
-        borderRadius: 10,
-        borderColor: "grey",
-        borderWidth: 2,
-        width: "50%",
-    },
-    body: {
-        fontSize: 30,
-        marginVertical: 15,
-        textAlign: "center"  
-    }
+  container: {
+    backgroundColor: 'skyblue',
+    margin: 10,
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 2,
+    width: '50%',
+  },
+  body: {
+    fontSize: 50,
+    marginVertical: 15,
+    textAlign: 'center',
+  },
 });
-export default Post
+export default Post;
